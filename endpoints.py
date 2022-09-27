b6e44229becb5fccb5ffdddc5fb1fbf4e7f5859b
g01d0v3 <nt8522247@gmail.com>
2022-09-27 22:19:45 +0300

    add(endpoints): added home, cheque, products page. created Products and Cheque BaseModels

---
+++ endpoints.py
@@ -0,0 +1,39 @@
+from typing import List
+import fastapi
+from pydantic import BaseModel
+from datetime import date
+
+api = fastapi.FastAPI()
+
+
+class Products(BaseModel):
+    product: str
+    price: float
+
+
+class Cheque(BaseModel):
+    id: int
+    date: date
+    products: List[Products]
+    provider: str = 'OOO Лысый Group'
+
+
+# def random_id():
+#     return ''.join(random.sample(string.digits, 8))
+
+
+@api.get('/')
+def __home_page():
+    return 'Welcome'
+
+
+@api.get('/{id}')
+def __cheque_page(id:  int, check: Cheque):
+    return Cheque
+
+
+@api.get('/products')
+def __products_page():
+    return "there's will be product list"
+
+
